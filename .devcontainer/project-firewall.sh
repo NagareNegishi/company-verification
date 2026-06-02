7e6532bd9c01848cffeab4fb994982996cde376a
Nagare Negishi <negishinaga@gmail.com>
2026-06-03 11:49:37 +1200

    Add firewall entry #5

--- .devcontainer/project-firewall.sh
+++ .devcontainer/project-firewall.sh
@@ -78,7 +78,8 @@ for domain in \
     "marketplace.visualstudio.com" \
     "vscode.blob.core.windows.net" \
     "update.code.visualstudio.com" \
-    "dotnetcli.blob.core.windows.net"; do
+    "dotnetcli.blob.core.windows.net" \
+    "api.business.govt.nz"; do
     echo "Resolving $domain..."
     ips=$(dig +noall +answer A "$domain" | awk '$4 == "A" {print $5}')
     if [ -z "$ips" ]; then
