2b4b01b036944a2182ea7bff7a5f16a5a4798931
Nagare Negishi <negishinaga@gmail.com>
2026-06-11 00:50:11 +1200

    option for key #7

---
+++ CompanyVerification.Core/Providers/Au/AbrOptions.cs
@@ -0,0 +1,16 @@
+namespace CompanyVerification.Core.Providers.Au;
+
+/// <summary>
+/// Configuration for the ABR adapter. Pass to <see cref="AbrProvider"/> at construction.
+/// </summary>
+public class AbrOptions
+{
+    /// <summary>
+    /// Authentication GUID issued by ABR on registration.
+    /// Each deployer must register their own GUID at
+    /// <see href="https://abr.business.gov.au/Documentation/WebServiceRegistration"/>.
+    /// Do not commit this value to source control.
+    /// </summary>
+    public required string Guid { get; init; }
+
+}
