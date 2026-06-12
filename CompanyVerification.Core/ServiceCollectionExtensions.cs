70846592e5ea05e3279a6b0d8b1c293819a543ea
Nagare Negishi <negishinaga@gmail.com>
2026-06-13 00:20:02 +1200

    refactor to take CompanyVerificationOptions #13

--- CompanyVerification.Core/ServiceCollectionExtensions.cs
+++ CompanyVerification.Core/ServiceCollectionExtensions.cs
@@ -13,15 +13,16 @@ public static class CompanyVerificationServiceCollectionExtensions
     /// Registers both country adapters, HTTP client support, and startup credential warnings.
     /// </summary>
     /// <param name="services">The service collection to add to.</param>
-    /// <param name="configureAbr">Configures <see cref="AbrOptions"/>. Set <c>Guid</c> from <c>ABR__Guid</c>.</param>
-    /// <param name="configureNzbn">Configures <see cref="NzbnOptions"/>. Set <c>SubscriptionKey</c> from <c>NZBN__SubscriptionKey</c>.</param>
+    /// <param name="configure">Configures credentials for all registered adapters.</param>
     public static IServiceCollection AddCompanyVerification(
         this IServiceCollection services,
-        Action<AbrOptions> configureAbr,
-        Action<NzbnOptions> configureNzbn)
+        Action<CompanyVerificationOptions> configure)
     {
-        services.Configure(configureAbr);
-        services.Configure(configureNzbn);
+        var opts = new CompanyVerificationOptions();
+        configure(opts);
+
+        services.Configure<AbrOptions>(o => o.Guid = opts.Abr.Guid);
+        services.Configure<NzbnOptions>(o => o.SubscriptionKey = opts.Nzbn.SubscriptionKey);
         services.AddHttpClient();
         services.AddSingleton<IVerificationProvider, AbrProvider>();
         services.AddSingleton<IVerificationProvider, NzbnProvider>();
