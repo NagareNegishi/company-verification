848fa5b57d8ccdcba1024667786d09f106fcb437
Nagare Negishi <negishinaga@gmail.com>
2026-06-13 00:20:58 +1200

    replace service #13

--- CompanyVerification.Api/Program.cs
+++ CompanyVerification.Api/Program.cs
@@ -1,21 +1,12 @@
 using CompanyVerification.Core;
-using CompanyVerification.Core.Providers.Au;
-using CompanyVerification.Core.Providers.Nz;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddHttpClient();
-
-// reads ABR__Guid from env
-builder.Services.Configure<AbrOptions>(builder.Configuration.GetSection("ABR"));
-
-// reads NZBN__SubscriptionKey from env
-builder.Services.Configure<NzbnOptions>(builder.Configuration.GetSection("NZBN"));
-
-// routing layer picks the right adapter at runtime
-builder.Services.AddSingleton<IVerificationProvider, AbrProvider>();
-builder.Services.AddSingleton<IVerificationProvider, NzbnProvider>();
+builder.Services.AddCompanyVerification(o =>
+{
+    o.Abr.Guid = builder.Configuration["ABR__Guid"] ?? string.Empty;
+    o.Nzbn.SubscriptionKey = builder.Configuration["NZBN__SubscriptionKey"] ?? string.Empty;
+});
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
